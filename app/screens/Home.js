417bde0bceeaa31a4817ad33939a11c37a37d8f3
Umer Riaz <umer.slysol@gmail.com>
2024-02-01 16:07:59 +0500

    feat: created khata type and customer options in home screen

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,11 +5,12 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import colors from "../config/colors";
 import Screen from "../components/Screen";
 import Form from "../components/form/Form";
+import Text from "../components/Text";
 import TextField from "../components/form/TextField";
 
 function Home(props) {
   return (
-    <Screen style={{ padding: 10 }}>
+    <Screen style={styles.container}>
       <View
         style={{
           flexDirection: "row",
@@ -27,7 +28,7 @@ function Home(props) {
               icon="magnify"
               name="global_search"
               onIconPress={() => console.log("pressed")}
-              placeholder="search here"
+              placeholder="Search here..."
               style={{ borderRadius: 40, paddingLeft: 20, height: 50 }}
             />
           </View>
@@ -39,12 +40,100 @@ function Home(props) {
           resizeMode="cover"
         />
       </View>
+      <View>
+        <Text style={styles.createKhataText}>Create a new khata</Text>
+        <View style={styles.selectKhataOuter}>
+          <View style={styles.allKhataTypesOuter}>
+            <View style={styles.khataTypeOuter}>
+              <MaterialCommunityIcons
+                name="scissors-cutting"
+                size={35}
+                color={colors.primary}
+              />
+              <Text style={styles.khataTypeText}>Tailor</Text>
+            </View>
+            <View style={styles.khataTypeOuter}>
+              <MaterialCommunityIcons
+                name="account-box"
+                size={35}
+                color={colors.primary}
+              />
+              <Text style={styles.khataTypeText}>Accounts</Text>
+            </View>
+          </View>
+          <View style={styles.addNewKhataType}>
+            <MaterialCommunityIcons
+              size={40}
+              name="plus-circle-outline"
+              color={colors.primary}
+            />
+          </View>
+        </View>
+      </View>
+      <View>
+        <Text style={styles.createKhataText}>Manage your customers</Text>
+        <View style={styles.selectKhataOuter}>
+          <View style={styles.allKhataTypesOuter}>
+            <View style={styles.khataTypeOuter}>
+              <MaterialCommunityIcons
+                name="account-plus"
+                size={35}
+                color={colors.primary}
+              />
+              <Text style={styles.khataTypeText}>Add Customer</Text>
+            </View>
+            <View style={styles.khataTypeOuter}>
+              <MaterialCommunityIcons
+                name="account-eye"
+                size={35}
+                color={colors.primary}
+              />
+              <Text style={styles.khataTypeText}>View Customers</Text>
+            </View>
+          </View>
+        </View>
+      </View>
     </Screen>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  addNewKhataType: {
+    backgroundColor: colors.white,
+    borderRadius: 50,
+  },
+  allKhataTypesOuter: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  container: {
+    padding: 10,
+    gap: 20,
+  },
+  createKhataText: {
+    marginBottom: 10,
+  },
+  khataTypeText: {
+    fontSize: 14,
+  },
+  khataTypeOuter: {
+    alignItems: "center",
+    borderRadius: 10,
+    backgroundColor: colors.white,
+    flex: 1,
+    gap: 10,
+    justifyContent: "center",
+    padding: 10,
+    width: 50,
+  },
+  selectKhataOuter: {
+    alignItems: "center",
+    backgroundColor: colors.primary,
+    borderRadius: 25,
+    gap: 20,
+    padding: 20,
+    width: "100%",
+  },
 });
 
 export default Home;
