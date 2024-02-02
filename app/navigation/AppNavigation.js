8fb51e83c3474397337f23d8bee60aad35a29da6
Umer Riaz <umer.slysol@gmail.com>
2024-02-02 20:21:27 +0500

    refactor: customize code for app navigation

--- app/navigation/AppNavigation.js
+++ app/navigation/AppNavigation.js
@@ -5,6 +5,28 @@ import Home from "../screens/Home";
 import colors from "../config/colors";
 
 const Tab = createBottomTabNavigator();
+const tabsData = [
+  {
+    name: "Home",
+    component: Home,
+    icon: "home",
+  },
+  {
+    name: "Khata",
+    component: Home,
+    icon: "book-open-blank-variant",
+  },
+  {
+    name: "Customers",
+    component: Home,
+    icon: "account-group",
+  },
+  {
+    name: "Account",
+    component: Home,
+    icon: "cog",
+  },
+];
 
 function AppNavigation() {
   return (
@@ -15,50 +37,21 @@ function AppNavigation() {
         tabBarActiveTintColor: colors.primary,
       }}
     >
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarIcon: ({ size, color }) => (
-            <MaterialCommunityIcons name="home" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Khata"
-        component={Home}
-        options={{
-          tabBarIcon: ({ size, color }) => (
-            <MaterialCommunityIcons
-              name="book-open-blank-variant"
-              size={size}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Customers"
-        component={Home}
-        options={{
-          tabBarIcon: ({ size, color }) => (
-            <MaterialCommunityIcons
-              name="account-group"
-              size={size}
-              color={color}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Account"
-        component={Home}
-        options={{
-          tabBarIcon: ({ size, color }) => (
-            <MaterialCommunityIcons name="cog" size={size} color={color} />
-          ),
-        }}
-      />
+      {tabsData.map((tab) => (
+        <Tab.Screen
+          name={tab.name}
+          component={tab.component}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <MaterialCommunityIcons
+                name={tab.icon}
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
+      ))}
     </Tab.Navigator>
   );
 }
