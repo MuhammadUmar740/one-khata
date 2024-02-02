772723ae9a104f66297344328adb84d3de445f09
Umer Riaz <umer.slysol@gmail.com>
2024-02-02 20:14:42 +0500

    feat: added navigation icons at app navigation

--- app/navigation/AppNavigation.js
+++ app/navigation/AppNavigation.js
@@ -24,6 +24,41 @@ function AppNavigation() {
           ),
         }}
       />
+      <Tab.Screen
+        name="Khata"
+        component={Home}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons
+              name="book-open-blank-variant"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Customers"
+        component={Home}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons
+              name="account-group"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Home}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <MaterialCommunityIcons name="cog" size={size} color={color} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
